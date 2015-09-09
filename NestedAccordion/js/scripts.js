ddef7cefe869365c3737dde23eb983befd0a9aad
Jonathan Creasman <jmcreasman89@gmail.com>
2015-09-09 18:15:10 -0400

    Update scripts.js

--- NestedAccordion/js/scripts.js
+++ NestedAccordion/js/scripts.js
@@ -1,1 +1,19 @@
+What does it do?
+On this page the javascript opens and closes the accordions on the pages and presents the information displayed there from the html and css pages.
+What interactive elements exist?
+As stated above this page interacts when the user clicks on the arrowed headers by opening the accorion folder and displaying the contents. It must also close it when clicked again.
+How do you interact with them?
+You interact with them by clicking on the different headers.
+What visual effects are produced by interacting?
+The content in the accordion folder is displayed and a different arrow image is displayed as well to cue in the user that that folder is open.
+What existing HTML elements are changed?
+The arrow picture is changed.
+What new HTML elements are created?
+A new arrow image is presented and the body copy is displayed.
+What CSS styles are used to produce the effect?
+The copy color and font is from the css. The css also helps devide the js and html code into classes and divs.
 
+//When the user clicks the header open the accordion
+//Display copy
+//When the user clicks the header again, close the accordion
+//This code along with any nessary html and css code will be grouped into its own class id so that it will "activate" when hat class id is "called on"
