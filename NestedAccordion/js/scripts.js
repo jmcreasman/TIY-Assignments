c8af6fbf7fadd9a8add0c712ca323072142559a9
Jon <jmcreasman89@gmail.com>
2015-09-12 18:34:43 -0400

    cleaning up code

--- NestedAccordion/js/scripts.js
+++ NestedAccordion/js/scripts.js
@@ -1,109 +1,32 @@
-//Function that opens accordion when clicked
-//I think the right is called cbp-nttrigger
-//
-// var heading = document.querySelector(".cbp-nttrigger");
-//
-// heading.addEventListener('click', function(){
-// document.querySelector("li").className = "cbp-ntopen";
-// });
+var clickAction = function(eventObject) { //
+  // The eventObject represents a mouse click event.  The event has a property (currentTarget (in our case h3/h4.cbp-nttrigger)) that represents the
+  // DOM element for which the event applies (i.e., the listener target).  The parentElement of the event target
+  // element is the li tag we seek.
+  var liElement = eventObject.currentTarget.parentElement;
+if (liElement.className === "cbp-ntopen") { //if the li element has class of cbp-ntopen when clicked, remove that class (closing the accordion)
+  liElement.className = null;
+} else { //otherwise give the li element the class of cbp-ntopen when clicked (opening the accordion)
+  liElement.className = "cbp-ntopen";
+}
+return true;
+}
 
-/*assigned name "heading" to the method of identifying the first element with class="cbp-nttrigger"
-and then adding the 'click' event listener to it so that when it is clicked it assigns
-the first 'li' it finds the class name 'cbp-ntopen' which according to the css will
-open the accordion.
-THAT CODE IS THE FOLLOWING 4 LINES
-var heading = document.querySelector(".cbp-nttrigger");
- heading.addEventListener('click', function(){
- document.querySelector("li").className = "cbp-ntopen";
-});
-*/
+var headings = document.querySelectorAll("h3.cbp-nttrigger"); //returns an array of all h3 headings class of cbp-nttrigger. These are assigned to var headings
+for (var i = 0; i < headings.length; i++) { //this for loop passes through each item in the array
+  var heading = headings[i];
+  heading.addEventListener('click', clickAction); //each item passing through the loop is given the event listener click and the function clickAction
+}
 
-
-var headings = document.querySelectorAll("h3.cbp-nttrigger")
-console.log("I am here!");
+//these lines of code do the exact same thing but for all h4 elements with a class of cbp-nttrigger
+var headings = document.querySelectorAll("h4.cbp-nttrigger");
 for (var i = 0; i < headings.length; i++) {
   var heading = headings[i];
-  heading.addEventListener('click', function(eventObject){
-    var liElement = eventObject.currentTarget.parentElement;
-  if (liElement.className === "cbp-ntopen") {
-    liElement.className = null;
-  } else {
-    liElement.className = "cbp-ntopen";
-  }
-  return true;
-  });
-
+  heading.addEventListener('click', clickAction);
 }
 
-var headings = document.querySelectorAll("h4.cbp-nttrigger")
+//these lines of code do the exact same thing but for any future h5 accordions we'd like to put inside our nested accordions (accordception!)
+var headings = document.querySelectorAll("h5.cbp-nttrigger");
 for (var i = 0; i < headings.length; i++) {
   var heading = headings[i];
-  heading.addEventListener('click', function(eventObject){
-    var liElement = eventObject.currentTarget.parentElement;
-  if (liElement.className === "cbp-ntopen") {
-    liElement.className = null;
-  } else {
-    liElement.className = "cbp-ntopen";
-  }
-  return true;
-  });
-
+  heading.addEventListener('click', clickAction);
 }
-
-
-  /*
-if (document.querySelector("li").className !== "cbp-ntopen"){ //if the li does not have a class name of cbp-ntopen do the following
-    document.querySelector("li").className = "cbp-ntopen"; //give it the class name cbp-ntopen
-    return true; //accordion opens
-}
-if (document.querySelector("li").className === "cbp-ntopen") { //if that li has class name cbp-ntopen and is clicked
-    document.querySelector("li").className = null; //remove that class name
-    return true; //accordion closes
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// What does it do?
-// On this page the javascript opens and closes the accordions on the pages and presents the information displayed there from the html and css pages.
-// What interactive elements exist?
-// As stated above this page interacts when the user clicks on the arrowed headers by opening the accorion folder and displaying the contents. It must also close it when clicked again.
-// How do you interact with them?
-// You interact with them by clicking on the different headers.
-// What visual effects are produced by interacting?
-// The content in the accordion folder is displayed and a different arrow image is displayed as well to cue in the user that that folder is open.
-// What existing HTML elements are changed?
-// The arrow picture is changed.
-// What new HTML elements are created?
-// A new arrow image is presented and the body copy is displayed.
-// What CSS styles are used to produce the effect?
-// The copy color and font is from the css. The css also helps devide the js and html code into classes and divs.
-
-//When the user clicks the header open the accordion
-//Display copy
-//When the user clicks the header again, close the accordion
-//This code along with any nessary html and css code will be grouped into its own class id so that it will "activate" when hat class id is "called on"
-//Some kind of if/else statement to do this?
-//if (user clicks on header) {
-//open accordion;
-//}else {
-//leave closed;
-//}
-//Then another similar statement to close it
-//if (user clicks on header) {
-//close accordion;
-//}else {
-//leave open;
-//}
-//I'm still fuzzy on how the DOM commands work but I believe they allow the user to interact with the web browser using the javascript so the js code that recognises when a user has clicked on a header will need to use some of them.
